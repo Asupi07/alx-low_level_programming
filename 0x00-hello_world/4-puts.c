03fa2c9d65e42770afe3ef53ba1126f4bbb30cab
Asupi07 <odhiambosaline17@gmail.com>
2022-10-14 16:01:56 -0700

    update

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * descriction: 'the program'
+ *
+ * return: always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
