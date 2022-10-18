7b5e43be2f3348b13a79376d21554617f6ce4f0f
Asupi07 <odhiambosaline17@gmail.com>
2022-10-18 10:50:05 -0700

    update

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints all digits
+ *
+ * Description - this program prints " All digits"
+ * Return : 0
+ */
+int main(void)
+{
+	int i;
+for (i = 0; i < 10; i++)
+{
+	printf("%i", i);
+}
+printf("\n");
 
+return (0);
+}
